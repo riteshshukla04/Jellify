3312a5fe808aa9cd9ab4d84cb85727b6f6484e63
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-20 20:48:53 -0600

    oh

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: [
+    'module:@react-native/babel-preset',
+    'babel-preset-expo'
+  ],
   plugins: [
-    'babel-preset-expo',
     // react-native-reanimated/plugin has to be listed last
     'react-native-reanimated/plugin',
   ]
