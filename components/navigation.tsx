a50b62d0728c9e2c396887181d98aa46e38437d8
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-21 14:14:06 -0600

    maybe I need this?

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -3,6 +3,7 @@ import Player from "./Player/stack";
 import { Tabs } from "./tabs";
 import { StackParamList } from "./types";
 import DetailsScreen from "./ItemDetail/screen";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Navigation(): React.JSX.Element {
 
@@ -11,6 +12,8 @@ export default function Navigation(): React.JSX.Element {
   console.debug("Rendering navigational tree...")
   
     return (
+      <SafeAreaView>
+
       <RootStack.Navigator>
           <RootStack.Screen 
             name="Tabs" 
@@ -18,15 +21,16 @@ export default function Navigation(): React.JSX.Element {
             options={{
               headerShown: false
             }}
-          />
+            />
           <RootStack.Screen 
             name="Player" 
             component={Player} 
             options={{
-               headerShown: false,
-               presentation: 'modal'
+              headerShown: false,
+              presentation: 'modal'
             }}
-          />
+            />
     </RootStack.Navigator>
+            </SafeAreaView>
     )
 }
