56ded1abc7391873de5f29ba764eb3de589846ef
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-20 20:45:30 -0600

    im missing this

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-
+    'babel-preset-expo',
     // react-native-reanimated/plugin has to be listed last
     'react-native-reanimated/plugin',
   ]
