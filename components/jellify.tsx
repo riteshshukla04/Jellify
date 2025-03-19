d14fabf0006dc5b3638bfd53dcad13dee7146bfa
Violet Caulfield <violet@cosmonautical.cloud>
2025-03-19 18:45:32 -0500

    remove toast stuff?

--- components/jellify.tsx
+++ components/jellify.tsx
@@ -1,28 +1,22 @@
 import _ from "lodash";
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import Navigation from "./navigation";
 import Login from "./Login/component";
 import { JellyfinAuthenticationProvider } from "./Login/provider";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { JellifyDarkTheme, JellifyLightTheme } from "./theme";
 import { PlayerProvider } from "../player/provider";
-import { useColorScheme } from "react-native";
-import { PortalProvider } from "@tamagui/portal";
 import { JellifyProvider, useJellifyContext } from "./provider";
 import { ToastProvider } from "@tamagui/toast";
-import SafeToastViewport from "./Global/components/toast-area-view-port";
 
 export default function Jellify(): React.JSX.Element {
 
   return (
-    <PortalProvider shouldAddRootHost>
-      <ToastProvider burntOptions={{ from: 'top'}}>
+    // <PortalProvider shouldAddRootHost>
+      // <ToastProvider burntOptions={{ from: 'top'}}>
         <JellifyProvider>
           <App />
         </JellifyProvider>
-      </ToastProvider>
-    </PortalProvider>
+      // </ToastProvider>
+    // </PortalProvider>
   );
 }
 
