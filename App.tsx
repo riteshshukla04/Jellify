398e6f1b64bfd9a57affb3f3370fe3192cbf15d1
Violet Caulfield <violet@cosmonautical.cloud>
2025-03-19 18:44:20 -0500

    okay?

--- App.tsx
+++ App.tsx
@@ -73,7 +73,7 @@ export default function App(): React.JSX.Element {
           </GestureHandlerRootView>
         </PersistQueryClientProvider>
 
-        <SafeToastViewport />
+        {/* <SafeToastViewport /> */}
 
       </NavigationContainer>
     </SafeAreaProvider>
